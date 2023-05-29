d12e984a069d3f7d2181b633358fec8dd655cb72
khalid <10abha10@gmail.com>
2023-05-29 19:16:09 +0300

    done

---
+++ mainProject/main_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+app_name="main_app"
+urlpatterns = [
+    path("",views.home_page,name="home_page"),
+    path('add/',views.add_page,name="add_page")
+]
