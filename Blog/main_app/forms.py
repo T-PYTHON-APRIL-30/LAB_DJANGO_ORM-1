00149337b770b1a1c2b97a7da767f3ff10817930
lena <lenaashayban@gmail.com>
2023-05-30 10:05:47 +0300

    lab22

--- Blog/main_app/forms.py
+++ Blog/main_app/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class Form(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['Title', 'Content', 'publish_date']
+        fields = ['Title', 'Content', 'publish_date', 'is_published']
