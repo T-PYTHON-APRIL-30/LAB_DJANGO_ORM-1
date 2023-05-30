e0e1c35101ce72a4eddcc9c0c87b78aa5b95c967
lena <lenaashayban@gmail.com>
2023-05-30 04:20:37 +0300

    lab22

---
+++ Blog/main_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from datetime import datetime
+
+# Create your models here.
+
+class Post(models.Model):
+    Title = models.CharField(max_length=100)
+    Content = models.TextField()
+    is_published = models.BooleanField(default=False)
+    publish_date = models.DateField(default=datetime.now)
+
+def __str__(self):
+    return self.title
---
+++ Blog/main_app/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render, redirect
+from .models import Post
+from .forms import Form
+
+# Create your views here.
+
+def home(request):
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+def add_post(request):
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = Form()
+    return render(request, 'add_post.html', {'form': form})
