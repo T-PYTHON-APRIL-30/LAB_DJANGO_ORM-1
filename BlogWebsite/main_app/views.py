1685be86dd263dfdb70878431a33c466cf79ed12
juhaina <jh75n.awad@gmail.com>
2023-05-31 01:18:13 +0300

    Finish - Juhaina

--- BlogWebsite/main_app/views.py
+++ BlogWebsite/main_app/views.py
@@ -11,5 +11,36 @@ def add_blog(request:HttpRequest):
     return render (request, "main_app/add_blog.html")
 
 def read_blog(request:HttpRequest):
-    read_blog = Post.objects.filter(is_published=True)
+    read_blog = Post.objects.all()
     return render(request, "main_app/read_blog.html", {"read_blog" : read_blog})
+
+def detail_blog(request:HttpRequest, i_id):
+    detail_blo = Post.objects.get(id = i_id)
+    return render(request, "main_app/detail_blog.html", {"detail_blo" : detail_blo})
+
+
+def update_blog(request:HttpRequest, i_id):
+    blog = Post.objects.get(id=i_id)
+    iso_date = blog.publish_date.isoformat()
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.content = request.POST["content"]
+        blog.publish_date = request.POST["publish_date"]
+        blog.is_published = request.POST["is_published"]
+        blog.save()
+        return redirect("main_app:detail_blog",  i_id = blog.id)
+     
+    return render (request, "main_app/update_blog.html", {'blog':blog, "iso_date" : iso_date})
+
+
+def delete_blog (request:HttpRequest, i_id):
+    blog = Post.objects.get(id=i_id)
+    blog.delete()
+
+    return redirect("main_app:read_blog")
+
+def search_page(request:HttpRequest):
+    search_phrase = request.GET.get("search", "")
+    blogs = Post.objects.filter(title__contains=search_phrase,)
+
+    return render(request, "main_app/search_page.html", {"blogs" : blogs})
--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# LAB_DJANGO_ORM-1
-
-## Using what you learned , Create a Blog website .
-- Users can read the blog posts.
-- Users can Add a blog post. 
+# LAB_DJANGO_ORM-2
 
 
-### Create model Post , it should have the following attributes :
-- Title : char field
-- Content : text field
-- is_published : boolean field
-- publish_date : datetime field
+## using your previous LAB "LAB_DJANGO_ORM-1" , do the following:
+- Add detail page for posts.
+- Add update post page.
+- Add delete post.
+- Add search functionalty (use an form / input in the navbar) , to let user search for posts based on the title (filter).
+- Only show pages that are published.
+
+
+## Bonus
+- handle detail page for non existent post, use try/except and display a Not Found page.
+
