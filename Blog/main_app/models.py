0de4f210dea543dda2827d1f2e6bdc03ed6acd33
lena <lenaashayban@gmail.com>
2023-05-31 05:07:55 +0300

    lab22

--- Blog/main_app/models.py
+++ Blog/main_app/models.py
@@ -7,4 +7,4 @@ class Post(models.Model):
     Title = models.CharField(max_length=100)
     Content = models.TextField()
     is_published = models.BooleanField(default=False)
-    publish_date = models.DateField(default=datetime.now)
+    publish_date = models.DateField()
