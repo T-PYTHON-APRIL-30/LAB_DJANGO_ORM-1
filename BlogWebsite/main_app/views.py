732169636d37af4b3a68a682c90daa6ba5a79ee5
juhaina <jh75n.awad@gmail.com>
2023-06-04 11:37:09 +0300

    orm2

--- BlogWebsite/main_app/views.py
+++ BlogWebsite/main_app/views.py
@@ -5,11 +5,42 @@ from .models import Post
 
 def add_blog(request:HttpRequest):
     if request.method == "POST":
-            new_blog = Post(title=request.POST["title"], content=request.POST["content"], is_published=request.POST["is_published"], publish_date=request.POST["publish_date"])
+            new_blog = Post(title=request.POST["title"], content=request.POST["content"], is_published=request.POST["is_published"], publish_date=request.POST["publish_date"],img=request.FILES["img"])
             new_blog.save()
             return redirect("main_app:read_blog")
     return render (request, "main_app/add_blog.html")
 
 def read_blog(request:HttpRequest):
-    read_blog = Post.objects.filter(is_published=True)
+    read_blog = Post.objects.all()
     return render(request, "main_app/read_blog.html", {"read_blog" : read_blog})
+
+def detail_blog(request:HttpRequest, i_id):
+    detail_blo = Post.objects.get(id = i_id)
+    return render(request, "main_app/detail_blog.html", {"detail_blo" : detail_blo})
+
+
+def update_blog(request:HttpRequest, i_id):
+    blog = Post.objects.get(id=i_id)
+    iso_date = blog.publish_date.isoformat()
+    if request.method == "POST":
+        blog.title = request.POST["title"]
+        blog.content = request.POST["content"]
+        blog.publish_date = request.POST["publish_date"]
+        blog.is_published = request.POST["is_published"]
+        blog.img = request.FILES["img"]
+        blog.save()
+        return redirect("main_app:detail_blog",  i_id = blog.id)
+     
+    return render (request, "main_app/update_blog.html", {'blog':blog, "iso_date" : iso_date})
+
+
+def delete_blog (request:HttpRequest, i_id):
+    blog = Post.objects.get(id=i_id)
+    blog.delete()
+    return redirect("main_app:read_blog")
+
+def search_page(request:HttpRequest):
+    search_phrase = request.GET.get("search", "")
+    blogs = Post.objects.filter(title__contains=search_phrase,)
+
+    return render(request, "main_app/search_page.html", {"blogs" : blogs})
